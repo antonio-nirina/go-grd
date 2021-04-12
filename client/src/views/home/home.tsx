7ff205f881ba9d60fc46e507fc7c2c5a69ec66c4
antonio-nirina <nirinahantonio@gmail.com>
2021-04-12 08:21:16 +0300

    render router

---
+++ client/src/views/home/home.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+const Home: React.FC = function() {
+  return(
+    <div>
+      <div className="main">
+        <div className="background">
+        	Home
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
