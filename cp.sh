d8ca7ac165b63a7b16de31ece3d4cd468fe19ef6
antonio-nirina <nirinahantonio@gmail.com>
2021-04-14 12:49:07 +0300

    sh

--- cp.sh
+++ cp.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 dir="${HOME}/Documents/faceit/new-project/grind/front-office/client"
+server="${HOME}/Documents/faceit/new-project/grind/front-office/server"
 orginame="${HOME}/Documents/faceit/new-project/go-example/client"
+org_server="${HOME}/Documents/faceit/new-project/go-example/server"
 
 if [ -d ${dir} ] ; then
-    rm -rf $dir
+    rm -rf $dir 
     cp -r ${orginame} "${HOME}/Documents/faceit/new-project/grind/front-office/"
 else 
     cp -r ${orginame} "${HOME}/Documents/faceit/new-project/grind/front-office/"
 fi
 
- ls ${dir}
+if [  -d ${server} ] ; then
+    rm -rf $server
+    cp -r ${org_server} "${HOME}/Documents/faceit/new-project/grind/front-office/"
+else 
+    cp -r ${org_server} "${HOME}/Documents/faceit/new-project/grind/front-office/"
+fi
+
+ echo "server and client copy with success"
