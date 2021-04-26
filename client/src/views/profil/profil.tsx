b4c62d6e89120bb74222ef55e28e2a04c3e5cc06
fabyxipe <fabien.rahajaniaina@xipe.com>
2021-04-26 22:35:18 +0200

    update profil.css + add ban profil test

--- client/src/views/profil/profil.tsx
+++ client/src/views/profil/profil.tsx
@@ -2,6 +2,7 @@ import React from "react"
 
 import Header from "../header/header"
 import Footer from "../footer/footer"
+import "../profil/profil.css"
 import "../../assets/css/style.css"
 
 
@@ -9,11 +10,30 @@ const Profil: React.FC = function() {
   return(
     <div className="profil">
       <div className="container">
-	      <Header/>	      
+	      <Header/>
+	      <div className="main-content">	      	
+	      	<div className="main">
+	      		<h1>Profil</h1>
+	      		<div className="avatar">
+	      			<div className="avatar-info">
+
+	      			</div>
+	      		</div>
+	      		<div className="premium">
+	      			<div className="premium-member">
+	      				
+	      			</div>
+	      		</div>
+	      		<div className="set">
+
+	      		</div>
+	      	</div>
+	      	<aside className="aside"></aside>
+	      </div>	      
 	      <Footer/>
 	  </div>
     </div>
-  );
+  )
 }
 
 export default Profil;
