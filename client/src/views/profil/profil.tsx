8ef1faf7a8f98d2d9414572b32c790c8d4eb884b
fabyxipe <fabien.rahajaniaina@xipe.com>
2021-04-27 23:38:16 +0200

    test ban + add content profil page

--- client/src/views/profil/profil.tsx
+++ client/src/views/profil/profil.tsx
@@ -3,6 +3,7 @@ import React from "react"
 import Header from "../header/header"
 import Footer from "../footer/footer"
 import "../profil/profil.css"
+import Avatar from "../../assets/image/avatar.png"
 import "../../assets/css/style.css"
 
 
@@ -13,12 +14,51 @@ const Profil: React.FC = function() {
 	      <Header/>
 	      <div className="main-content">	      	
 	      	<div className="main-pro">
-	      		<h1>Profil</h1>
-	      		<div className="avatar">
+	      		<div className="tabs">
+	      			<ul>
+	      				<li><a href="#">Mon mur</a></li>
+	      				<li><a href="#">Mes jeux</a></li>
+	      				<li><a href="#">Mes équipes</a></li>
+	      				<li><a href="#">Mes tournois</a></li>
+	      				<li><a href="#">Mes resultats</a></li>
+	      				<li><a href="#">Premium</a></li>
+	      			</ul>
+	      		</div>
+	      		<div className="wall">
+	      			<div className="avatar">
+		      			<p><img src = {Avatar} /></p>
+		      			<p><strong>Noob_021</strong></p>
+
+	      			</div>
 	      			<div className="avatar-info">
+	      				
+	      				<div className="table">
+	      					<div className="cell">
+	      						<p>Plateform</p>
+	      						<p><span>Playstation</span></p>
+	      					</div>
+	      					<div className="cell">
+	      						<p>Serveur location</p>
+	      						<p><span>Asia</span></p>
+	      					</div>
+	      					<div className="cell">
+	      						<p>Exp</p>
+	      						<p><span>5</span></p>
+	      					</div>
+	      					<div className="cell">
+	      						<p>Game duration</p>
+	      						<p><span>6 heures</span></p>
+	      					</div>
+	      				</div>
+	      				<div>
+	      				
+	      				</div>
+	      			</div>
+	      			<div className="stat">
 
 	      			</div>
 	      		</div>
+
 	      		<div className="premium">
 	      			<div className="premium-member">
 	      				
@@ -37,3 +77,4 @@ const Profil: React.FC = function() {
 }
 
 export default Profil;
+
