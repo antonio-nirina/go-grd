d74230fc8d613a98d833db5810f36c4b28f8cde7
microtten <fabien.rahajaniaina@gmail.com>
2021-04-28 15:48:23 +0200

    redux

--- client/src/views/auth/login.tsx
+++ client/src/views/auth/login.tsx
@@ -1,5 +1,9 @@
 import React from "react"
+import {Link} from 'react-router-dom'
 
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import {Field} from 'redux-form'
 import Header from "../header/header"
 import Slider from "../slider/slider"
 import Participate from "../participate/participate"
@@ -17,7 +21,23 @@ const Login: React.FC = function() {
       <div className="container">
 	      <Header/>
 	      <div className="main">
-	        
+	        <div className="content">
+				    <div className="wrap connexion formulaire">
+					    <div className="bloc noborder">
+							<h1 className="text-center">Connexion</h1>
+							<form>
+					          	<Field name="email" component={this.renderInput} type="text" placeholder="Email" />
+					          	<Field name="password" component={this.renderInput} type="password" label="Mot de passe" placeholder="Mot de passe" />
+					          	<div className="text-center bottom">
+					          		<button type="submit" className="btn bt btBlue" >Se connecter</button><br/>
+					          		Vous n'avez pas encore un compte?<br/>
+	                                 <Link to="/signup">Inscrivez vous!</Link><br/><br/>
+	                                 <Link to="/mot-de-passe-oublie">Mot de passe oublié?</Link>
+					          	</div>
+					        </form>
+					    </div>														        					   
+		    		</div>
+		    	</div>
 	      </div>
 	      <Footer/>
 	  </div>
