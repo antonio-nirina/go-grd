525a5f5f513405b4cd99b84c51e98ef5efb3219c
fabyxipe <fabien.rahajaniaina@xipe.com>
2021-04-29 00:30:02 +0200

    updated profil.css

--- client/src/views/profil/profil.tsx
+++ client/src/views/profil/profil.tsx
@@ -5,6 +5,7 @@ import Footer from "../footer/footer"
 import "../profil/profil.css"
 import Avatar from "../../assets/image/avatar.png"
 import "../../assets/css/style.css"
+import ReactDOM from "react-dom"
 
 
 const Profil: React.FC = function() {
@@ -14,72 +15,85 @@ const Profil: React.FC = function() {
 	      <Header/>
 	      <div className="main-content">	      	
 	      	<div className="main-pro">
-	      		<div className="tabs">
-	      			<ul>
-	      				<li><a href="#">Mon mur</a></li>
-	      				<li><a href="#">Mes jeux</a></li>
-	      				<li><a href="#">Mes équipes</a></li>
-	      				<li><a href="#">Mes tournois</a></li>
-	      				<li><a href="#">Mes resultats</a></li>
-	      				<li><a href="#">Premium</a></li>
-	      			</ul>
-	      		</div>
-	      		<div className="wall">
-	      			<div className="avatar">
-		      			<p><img src = {Avatar} /></p>
-		      			<p><strong>Noob_021</strong></p>
-	      			</div>
-	      			<div className="avatar-info">
-	      				
-	      				<div className="table">
-	      					<div className="cell">
-	      						<p>Plateform</p>
-	      						<p><span>Playstation</span></p>
-	      					</div>
-	      					<div className="cell">
-	      						<p>Serveur location</p>
-	      						<p><span>Asia</span></p>
-	      					</div>
-	      					<div className="cell">
-	      						<p>Exp</p>
-	      						<p><span>5</span></p>
-	      					</div>
-	      					<div className="cell">
-	      						<p>Game duration</p>
-	      						<p><span>6 heures</span></p>
-	      					</div>
-	      				</div>	      				
-	      			</div>
-	      			<div className="stat">
-	      				<h2>Mes stats</h2>
-	      				<p>Kill</p>
-	      				<p>Death</p>
-	      				<p>Assist</p>
-	      			</div>
-	      			<div className="percent">
-	      				<h2>Win rate</h2>
-	      				<p>85%</p>
-	      			</div>
-	      		</div>
-	      		<div className="about-me">
-	      			<div className="first">
-	      				<input type="text" placeholder="Changer de pseudo"/>
-	      				<input type="numéro" placeholder="Numéro de téléphone"/>
-	      				<div className="lang">
-	      					<p>Langue <span></span></p>
-	      				</div>
-	      			</div>
-	      			<div className="second">
-	      				<div className="audience">
-	      					<h2>Stats globale</h2>
-	      					<p>Nombre de victoire : <span>540</span></p>
-	      					<p>Nombre de défaite : <span>230</span></p>
-	      				</div>
-	      			</div>
-	      			<div className="third">
-	      				<div className="graph"></div>
-	      			</div>
-	      		</div> 
+	      		<div className="wall-bloc">
+		      		<div className="tabs">
+		      			<ul>
+		      				<li><a href="#" className="active">Mon mur</a></li>
+		      				<li><a href="#">Mes jeux</a></li>
+		      				<li><a href="#">Mes équipes</a></li>
+		      				<li><a href="#">Mes tournois</a></li>
+		      				<li><a href="#">Mes resultats</a></li>
+		      				<li><a href="#">Premium</a></li>
+		      			</ul>
+		      		</div>
+		      		<div className="wall">
+		      			<div className="avatar">
+			      			<p><img src = {Avatar} /></p>
+			      			<p className="pseudo"><strong>Noob_021</strong></p>
+		      			</div>
+		      			<div className="avatar-info">
+		      				
+		      				<div className="table">
+		      					<div className="cell">
+		      						<h2>Plateform</h2>
+		      						<p><span>Playstation</span></p>
+		      					</div>
+		      					<div className="cell">
+		      						<h2>Serveur location</h2>
+		      						<p><span>Asia</span></p>
+		      					</div>
+		      					<div className="cell">
+		      						<h2>Expérience</h2>
+		      						<p><span>5</span></p>
+		      					</div>
+		      					<div className="cell">
+		      						<h2>Game duration</h2>
+		      						<p><span>6 heures</span></p>
+		      					</div>
+		      				</div>	      				
+		      			</div>
+		      			<div className="stat">
+		      				<div className="float">
+		      					<h2>Mes stats</h2>
+		      				</div>
+		      				<div className="float">
+			      				<p>Victoire : <span>40%</span></p>
+			      				<div className="myProgress">    						
+		    						<div className="myBar" style={{width:"40%", background:"#6642a9"}}></div>    						
+		    					</div>
+		    					<p>Defaite : <span>25%</span></p>
+		    					<div className="myProgress">
+		    						<div className="myBar" style={{width:"25%", background:"#1da1f2"}}></div>    						
+		    					</div>
+		    					<p>Abandon : <span>75%</span></p>
+		    					<div className="myProgress">
+		    						<div className="myBar" style={{width:"75%", background:"#f9753d"}}></div>    						
+		    					</div>
+		    				</div>
+		      			</div>	      			
+		      		</div>
+		      	</div>
+		      	<div className="about-bloc">
+		      		<div className="about-me">
+		      			<div className="first">
+		      				<input type="text" placeholder="Changer de pseudo"/>
+		      				<input type="numéro" placeholder="Numéro de téléphone"/>
+		      				<div className="lang">
+		      					<p>Langue <span></span></p>
+		      				</div>
+		      			</div>
+		      			<div className="second">
+		      				<div className="audience">
+		      					<h2>Stats globale</h2>
+		      					<p>Nombre de victoire : <span>540</span></p>
+		      					<p>Nombre de défaite : <span>230</span></p>
+		      				</div>
+		      			</div>
+		      			<div className="third">
+		      				<div className="graph"></div>
+		      			</div>
+		      		</div>
+		      	</div>
 	      		<div className="my-games">
 	      			<h2>Mes jeux</h2>
 	      			<div className="img-game"></div>
@@ -101,8 +115,7 @@ const Profil: React.FC = function() {
 	      			<div className="img-game"></div>
 	      		</div>
 
-	      	</div>
-	      	<aside className="aside"></aside>
+	      	</div>	      	
 	      </div>	      
 	      <Footer/>
 	  </div>
