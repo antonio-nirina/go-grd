2e6d69ad5aa15a9b8acca323e868ce360ae2f70b
fabyxipe <fabien.rahajaniaina@xipe.com>
2021-04-28 20:20:51 +0200

    update profil page

--- client/src/views/profil/profil.tsx
+++ client/src/views/profil/profil.tsx
@@ -28,7 +28,6 @@ const Profil: React.FC = function() {
 	      			<div className="avatar">
 		      			<p><img src = {Avatar} /></p>
 		      			<p><strong>Noob_021</strong></p>
-
 	      			</div>
 	      			<div className="avatar-info">
 	      				
@@ -49,24 +48,59 @@ const Profil: React.FC = function() {
 	      						<p>Game duration</p>
 	      						<p><span>6 heures</span></p>
 	      					</div>
-	      				</div>
-	      				<div>
-	      				
-	      				</div>
+	      				</div>	      				
 	      			</div>
 	      			<div className="stat">
-
+	      				<h2>Mes stats</h2>
+	      				<p>Kill</p>
+	      				<p>Death</p>
+	      				<p>Assist</p>
+	      			</div>
+	      			<div className="percent">
+	      				<h2>Win rate</h2>
+	      				<p>85%</p>
 	      			</div>
 	      		</div>
-
+	      		<div className="about-me">
+	      			<div className="first">
+	      				<input type="text" placeholder="Changer de pseudo"/>
+	      				<input type="numéro" placeholder="Numéro de téléphone"/>
+	      				<div className="lang">
+	      					<p>Langue <span></span></p>
+	      				</div>
+	      			</div>
+	      			<div className="second">
+	      				<div className="audience">
+	      					<h2>Stats globale</h2>
+	      					<p>Nombre de victoire : <span>540</span></p>
+	      					<p>Nombre de défaite : <span>230</span></p>
+	      				</div>
+	      			</div>
+	      			<div className="third">
+	      				<div className="graph"></div>
+	      			</div>
+	      		</div> 
+	      		<div className="my-games">
+	      			<h2>Mes jeux</h2>
+	      			<div className="img-game"></div>
+	      		</div>
+	      		<div className="my-teams">
+	      			<h2>Mes équipes</h2>
+	      			<div className="img-game"></div>
+	      		</div>
+	      		<div className="mes-tournois">
+	      			<h2>Mes tournois</h2>
+	      			<div className="img-game"></div>
+	      		</div>
+	      		<div className="mes-resultats">
+	      			<h2>Mes résultats</h2>
+	      			<div className="img-game"></div>
+	      		</div>
 	      		<div className="premium">
-	      			<div className="premium-member">
-	      				
-	      			</div>
+	      			<h2>Premium</h2>
+	      			<div className="img-game"></div>
 	      		</div>
-	      		<div className="set">
 
-	      		</div>
 	      	</div>
 	      	<aside className="aside"></aside>
 	      </div>	      
