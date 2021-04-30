f90a23356b2e6afa9e0cd3e1ac33c1b5622bc175
fabyxipe <fabien.rahajaniaina@xipe.com>
2021-04-30 17:20:58 +0200

    updated slider game

--- client/src/views/profil/profil.tsx
+++ client/src/views/profil/profil.tsx
@@ -96,18 +96,18 @@ const Profil: React.FC = function() {
 	      		<div className="my-games">
 	      			<h2>Mes jeux</h2>
 	      			<div className="img-game">
-	      				<Carousel showArrows={false} autoPlay={true} interval={8000} infiniteLoop={true} showThumbs={false} transitionTime={1000}>
-					        <div><img src="https://i.ibb.co/ByGkhS1/apexlegend.jpg" alt="apexlegend" /></div>
-					      	<div><img src="https://i.ibb.co/Yd2v60Q/blackops.jpg" alt="blackops"/></div>
-					      	<div><img src="https://i.ibb.co/TK5JYMz/fifa21.jpg" alt="fifa21" /></div>
-					      	<div><img src="https://i.ibb.co/Dtym1JK/fortnite.jpg" alt="fortnite" /></div>
-					      	<div><img src="https://i.ibb.co/9VPnb7p/mwarfare.jpg" alt="mwarfare" /></div>
-					      	<div><img src="https://i.ibb.co/89xKdw2/rainbowsix-siege.jpg" alt="rainbowsix-siege"/></div>
-					      	<div><img src="https://i.ibb.co/CPDzC7n/rocketl.jpg" alt="rocketl" /></div>
-					      	<div><img src="https://i.ibb.co/8Y0r1NH/warzone.jpg" alt="warzone" /></div>
+	      				<Carousel swipeable={true} centerSlidePercentage={20} dynamicHeight={false} centerMode={true} showArrows={true} autoPlay={true} interval={8000} infiniteLoop={true} showThumbs={false} transitionTime={1000}>
+					        <div className="game-slide"><img src="https://i.ibb.co/ByGkhS1/apexlegend.jpg" alt="apexlegend" /></div>
+					      	<div className="game-slide"><img src="https://i.ibb.co/Yd2v60Q/blackops.jpg" alt="blackops"/></div>
+					      	<div className="game-slide"><img src="https://i.ibb.co/TK5JYMz/fifa21.jpg" alt="fifa21" /></div>
+					      	<div className="game-slide"><img src="https://i.ibb.co/Dtym1JK/fortnite.jpg" alt="fortnite" /></div>
+					      	<div className="game-slide"><img src="https://i.ibb.co/9VPnb7p/mwarfare.jpg" alt="mwarfare" /></div>
+					      	<div className="game-slide"><img src="https://i.ibb.co/89xKdw2/rainbowsix-siege.jpg" alt="rainbowsix-siege"/></div>
+					      	<div className="game-slide"><img src="https://i.ibb.co/CPDzC7n/rocketl.jpg" alt="rocketl" /></div>
+					      	<div className="game-slide"><img src="https://i.ibb.co/8Y0r1NH/warzone.jpg" alt="warzone" /></div>
 	    				</Carousel>
-	    				<div className="bt-container">
-	    					<a href="#">Ajouter</a>
+	    				<div className="bt-game-container">
+	    					<a href="#" className="btn bg-yellow">Ajouter jeux</a>
 	    				</div>
 	      			</div>
 	      		</div>
