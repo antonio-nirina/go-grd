26268a807cd59bfd50c531c42aba88bfa498681c
fabyxipe <fabien.rahajaniaina@xipe.com>
2021-05-14 20:43:53 +0200

    add tournament bloc style

--- client/src/views/tournament/tournament.tsx
+++ client/src/views/tournament/tournament.tsx
@@ -5,6 +5,9 @@ import Footer from "../footer/footer"
 
 import "../tournament/tournament.css"
 import "../../assets/css/style.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faPlaystation } from "@fortawesome/free-brands-svg-icons"
+import { faCalendarAlt, faInfoCircle, faGamepad, faTrophy, faMedal, faStepBackward, faStepForward, faChevronRight, faChevronLeft, faMobile } from "@fortawesome/free-solid-svg-icons"
 
 
 const Tournament: React.FC = function(props:any) {
@@ -13,49 +16,243 @@ const Tournament: React.FC = function(props:any) {
   	<div className="Tournament">
 		<div className="container">
 			<Header/>
-			<div className="full-container">
+			<div className="full-container">				
 				<h1>Liste des tournois disponibles</h1>
-				<h2>Tournois en direct (15)</h2>
-				<div className="bt-container"><Link to="#" className="btn bg-yellow">Voir plus</Link></div>
-				<div className="live side">					
-					<Link to="#">
-						<div className="items">
-							<img src="https://i.ibb.co/Vw39G5b/championship-rl.jpg" alt="championship-rl" className="imgresp"/>
-							<div className="side-infos">
-								<div className="meta">
-									<table>
-										<tr>
-											<th>Date</th>
-											<th>Equipe</th>
-											<th>Genre</th>
-											<th>Région</th>								
-										</tr>
-										<tr>
-											<td>19 Jan 17:00</td>
-											<td>12/16</td>
-											<td>5 ON 5</td>
-											<td>France</td>
-										</tr>
-									</table>								
+				<div className="card">
+					<h2>Tournois en direct (15)</h2>
+					<div className="bt-container">
+						<Link to="#" className="btn bg-yellow">Voir plus</Link>
+					</div>
+					<div className="live side">					
+						<Link to="/" title="">
+							<div className="items">
+								<div className="side-img">
+									<img src="https://i.ibb.co/Vw39G5b/championship-rl.jpg" alt="championship-rl"/>
 								</div>
-								<div className="name-section">
-									<p>Tournois Rocket League</p>
+								<div className="side-infos">
+									<div className="meta">
+										<table>
+											<tr>
+												<th>Date</th>
+												<th>Equipe</th>
+												<th>Genre</th>
+												<th>Région</th>								
+											</tr>
+											<tr>
+												<td>19 Jan 17:00</td>
+												<td>12/16</td>
+												<td>5 ON 5</td>
+												<td>France</td>
+											</tr>
+										</table>								
+									</div>
+									<div className="name-section">
+										<p>Tournois Rocket League</p>
+									</div>
+									<div className="prize-section">
+										<div className="prize-warp"><i className="awesome"><FontAwesomeIcon icon={faTrophy}/></i>Prix</div>
+										<div className="prize">-</div>
+									</div>								
 								</div>
-								<div className="prize-section">
-									<div className="prize-warp"><i></i>Prix</div>
-									<div className="prize">100€ Prix Cache</div>
-								</div>								
+								<div className="btn-full">
+									<Link to="/" className="signup-btn bg-yellow">Inscrivez vous</Link>
+								</div>
 							</div>
-							<div className="btn-full">
-								<a className="signup-btn bg-yellow">Inscrivez vous</a>
+						</Link>						
+					</div>	
+					<div className="live side">					
+						<Link to="/" title="">
+							<div className="items">
+								<div className="side-img">
+									<img src="https://i.ibb.co/Vw39G5b/championship-rl.jpg" alt="championship-rl"/>
+								</div>
+								<div className="side-infos">
+									<div className="meta">
+										<table>
+											<tr>
+												<th>Date</th>
+												<th>Equipe</th>
+												<th>Genre</th>
+												<th>Région</th>								
+											</tr>
+											<tr>
+												<td>19 Jan 17:00</td>
+												<td>12/16</td>
+												<td>5 ON 5</td>
+												<td>France</td>
+											</tr>
+										</table>								
+									</div>
+									<div className="name-section">
+										<p>Tournois Rocket League</p>
+									</div>
+									<div className="prize-section">
+										<div className="prize-warp"><i className="awesome"><FontAwesomeIcon icon={faTrophy}/></i>Prix</div>
+										<div className="prize">-</div>
+									</div>								
+								</div>
+								<div className="btn-full">
+									<Link to="/" className="signup-btn bg-yellow">Inscrivez vous</Link>
+								</div>
 							</div>
-						</div>
-					</Link>
-				</div>
-				<div className="upcoming side">
-					<div className="items"></div>
-				</div>
-			</div>
+						</Link>						
+					</div>
+					<div className="live side">					
+						<Link to="/" title="">
+							<div className="items">
+								<div className="side-img">
+									<img src="https://i.ibb.co/Vw39G5b/championship-rl.jpg" alt="championship-rl"/>
+								</div>
+								<div className="side-infos">
+									<div className="meta">
+										<table>
+											<tr>
+												<th>Date</th>
+												<th>Equipe</th>
+												<th>Genre</th>
+												<th>Région</th>								
+											</tr>
+											<tr>
+												<td>19 Jan 17:00</td>
+												<td>12/16</td>
+												<td>5 ON 5</td>
+												<td>France</td>
+											</tr>
+										</table>								
+									</div>
+									<div className="name-section">
+										<p>Tournois Rocket League</p>
+									</div>
+									<div className="prize-section">
+										<div className="prize-warp"><i className="awesome"><FontAwesomeIcon icon={faTrophy}/></i>Prix</div>
+										<div className="prize">-</div>
+									</div>								
+								</div>
+								<div className="btn-full">
+									<Link to="/" className="signup-btn bg-yellow">Inscrivez vous</Link>
+								</div>
+							</div>
+						</Link>						
+					</div>
+					</div>
+				<div className="card">
+					<h2>Tournois à venir (9)</h2>
+					<div className="bt-container">
+						<Link to="#" className="btn bg-yellow">Voir plus</Link>
+					</div>
+					<div className="upcomming side">					
+						<Link to="/" title="">
+							<div className="items">
+								<div className="side-img">
+									<img src="https://i.ibb.co/Vw39G5b/championship-rl.jpg" alt="championship-rl"/>
+								</div>
+								<div className="side-infos">
+									<div className="meta">
+										<table>
+											<tr>
+												<th>Date</th>
+												<th>Equipe</th>
+												<th>Genre</th>
+												<th>Région</th>								
+											</tr>
+											<tr>
+												<td>19 Jan 17:00</td>
+												<td>12/16</td>
+												<td>5 ON 5</td>
+												<td>France</td>
+											</tr>
+										</table>								
+									</div>
+									<div className="name-section">
+										<p>Tournois Rocket League</p>
+									</div>
+									<div className="prize-section">
+										<div className="prize-warp"><i className="awesome"><FontAwesomeIcon icon={faTrophy}/></i>Prix</div>
+										<div className="prize">100€ Prix Cache</div>
+									</div>								
+								</div>
+								<div className="btn-full">
+									<Link to="/" className="signup-btn bg-yellow">Inscrivez vous</Link>
+								</div>
+							</div>
+						</Link>						
+					</div>
+					<div className="upcomming side">					
+						<Link to="/" title="">
+							<div className="items">
+								<div className="side-img">
+									<img src="https://i.ibb.co/Vw39G5b/championship-rl.jpg" alt="championship-rl"/>
+								</div>
+								<div className="side-infos">
+									<div className="meta">
+										<table>
+											<tr>
+												<th>Date</th>
+												<th>Equipe</th>
+												<th>Genre</th>
+												<th>Région</th>								
+											</tr>
+											<tr>
+												<td>19 Jan 17:00</td>
+												<td>12/16</td>
+												<td>5 ON 5</td>
+												<td>France</td>
+											</tr>
+										</table>								
+									</div>
+									<div className="name-section">
+										<p>Tournois Rocket League</p>
+									</div>
+									<div className="prize-section">
+										<div className="prize-warp"><i className="awesome"><FontAwesomeIcon icon={faTrophy}/></i>Prix</div>
+										<div className="prize">100€ Prix Cache</div>
+									</div>								
+								</div>
+								<div className="btn-full">
+									<Link to="/" className="signup-btn bg-yellow">Inscrivez vous</Link>
+								</div>
+							</div>
+						</Link>						
+					</div>	
+					<div className="upcomming side">					
+						<Link to="/" title="">
+							<div className="items">
+								<div className="side-img">
+									<img src="https://i.ibb.co/Vw39G5b/championship-rl.jpg" alt="championship-rl"/>
+								</div>
+								<div className="side-infos">
+									<div className="meta">
+										<table>
+											<tr>
+												<th>Date</th>
+												<th>Equipe</th>
+												<th>Genre</th>
+												<th>Région</th>								
+											</tr>
+											<tr>
+												<td>19 Jan 17:00</td>
+												<td>12/16</td>
+												<td>5 ON 5</td>
+												<td>France</td>
+											</tr>
+										</table>								
+									</div>
+									<div className="name-section">
+										<p>Tournois Rocket League</p>
+									</div>
+									<div className="prize-section">
+										<div className="prize-warp"><i className="awesome"><FontAwesomeIcon icon={faTrophy}/></i>Prix</div>
+										<div className="prize">100€ Prix Cache</div>
+									</div>								
+								</div>
+								<div className="btn-full">
+									<Link to="/" className="signup-btn bg-yellow">Inscrivez vous</Link>
+								</div>
+							</div>
+						</Link>						
+					</div>		
+					</div>
+			</div>				
 			<Footer/>
 		</div>		
     </div>    
