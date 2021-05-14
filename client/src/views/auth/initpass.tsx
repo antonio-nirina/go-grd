797268e0d6ba4470abc533fc3d64448746f49911
microtten <fabien.rahajaniaina@gmail.com>
2021-05-14 15:18:51 +0200

    add initpass page

--- client/src/views/auth/initpass.tsx
+++ client/src/views/auth/initpass.tsx
@@ -16,7 +16,7 @@ import Footer from "../footer/footer"
 import joystick from "../../assets/image/joystick.png"
 import IconXbox from "../../assets/image/icon-xbox.png"
 import IconPs from "../../assets/image/playstation.png"
-import "../auth/login.css"
+import "../auth/initpass.css"
 import "../../assets/css/style.css"
 
 type Inputs = {
@@ -24,7 +24,7 @@ type Inputs = {
 	email:string
 }
 
-const Login: React.FC = function() {
+const InitPass: React.FC = function() {
 	const history = useHistory()
 	const dispatch = useDispatch()
 	const { register, handleSubmit, formState: { errors } } = useForm<Inputs>()
@@ -53,29 +53,27 @@ const Login: React.FC = function() {
 	}
 
   return(
-	<div className="login">
+	<div className="login initpass">
 		<div className="container">
 			<Header0/>
 			<div className="main">
 				<div className="containt">
 					<div className="group">
 					<h1>
-						Connexion
+						Réinitialisation mot de passe
 						<img src={joystick} alt=""/>
 					</h1>
 						<div>
 							<span style={{"color":"red"}}>{errorForm ? Translation("fr").login.errorForm : ""}</span>
 						</div>
+						<div className="alert alert-success text-center invisible">Un mail de changement de mot de passe vous a été envoyé</div>
 						<form onSubmit={handleSubmit(onSubmit)}>
-							<input className="mgt10" type = "email" placeholder = "Ton email" {...register("email", { required: true })} name="email" />
-							<input type ="password" placeholder ={Translation("fr").login.password}  {...register("password", { required: true })} name="password" />
+							<input className="mgt10" type = "email" placeholder = "Ton email" {...register("email", { required: true })} name="email" required/>							
 							<button className="btn bg-yellow mg15">
-								Se connecter
+								Réinitialiser mon mot de passe
 							</button>
-						</form>
-						<div className="infos">
-							<p className="mb15">Vous n'avez pas encore de compte ? <Link to = "/register" title="Inscrivez-vous" className="italic cl-yellow">Inscrivez-vous !</Link></p>
-							<p className="mb15"><Link to ="/initpass" title="Mot de passe oublié ?" className="italic cl-yellow">Mot de passe oublié ?</Link></p>
+						</form>						
+						<div className="infos">							
 							<div className="other-account">
 								<p>Connectez-vous avec votre compte : </p>
 								<span onClick={Siging}><img src={IconXbox} alt="xbox" /></span>
@@ -91,4 +89,4 @@ const Login: React.FC = function() {
 	)
 }
 
-export default Login;
+export default InitPass;
