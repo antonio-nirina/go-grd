dda6317100c72ee168a8301b6322619f8856078c
fabyxipe <fabien.rahajaniaina@xipe.com>
2021-05-26 22:08:11 +0200

    fix router

--- client/src/views/annexe/profile.tsx
+++ client/src/views/annexe/profile.tsx
@@ -1,46 +1,34 @@
 import React from "react"
-import { Route, Switch } from "react-router-dom"
-import Home from "../home/home"
-import Ligue from "../ligue/ligue"
-import Tournament from "../tournament/tournament"
-import Info from "../tournament/info"
-import Matches from "../tournament/matches"
-import Teams from "../tournament/teams"
-import Rules from "../tournament/rules"
-import Wager from "../wager/wager"
-import Assistant from "../assistant/assistant"
-import Communaute from "../communaute/communaute"
-import Login from "../auth/login"
-import Profil from "../profil/profil"
-import Profile from "../annexe/profile"
-import Inscription from "../auth/inscription"
-import InitPass from "../auth/initpass"
-import UpdatePassword from "../auth/updatePassword"
-import ProtectedRoute from "./protectedRoute"
-import Register from "../auth/register"
+import { useSelector } from "react-redux"
 
-const Router = function() {
-	return (
-		<Switch>
-			<Route path="/" exact component={Home} />
-			<ProtectedRoute path="/tournament" exact component={Tournament} />
-			<ProtectedRoute path="/info" exact component={Info} />
-			<ProtectedRoute path="/matches" exact component={Matches} />
-			<ProtectedRoute path="/teams" exact component={Teams} />
-			<ProtectedRoute path="/rules" exact component={Rules} />
-			<ProtectedRoute path="/ligue" exact component={Ligue} />
-			<ProtectedRoute path="/wager" exact component={Wager} />
-			<ProtectedRoute path="/communaute" exact component={Communaute} />
-			<ProtectedRoute path="/assistant" exact component={Assistant} />
-			<Route path="/login" exact component={Login} />
-			<Route path="/inscription" exact component={Inscription} />
-			<Route path="/register" exact component={Register} />
-			<Route path="/forgot-password" exact component={InitPass} />
-			<Route path="/update-password" exact component={UpdatePassword} />
-			<ProtectedRoute path="/profil" exact component={Profil} />
-			<ProtectedRoute path="/profile" exact component={Profile} />
-		</Switch>
-	)
+// import { faXbox, faPlaystation } from "@fortawesome/free-brands-svg-icons"
+// faCogs, faCalendarAlt, faInfoCircle, faGamepad, faTrophy, faMedal, faStepBackward, faStepForward, faChevronRight, faChevronLeft, faMobile,
+
+import Header from "../header/header"
+import Footer from "../footer/footer"
+import "../annexe/profile.css"
+import "../../assets/css/style.css"
+
+// import { CircularProgressbar, CircularProgressbarWithChildren, buildStyles} from "react-circular-progressbar"
+
+import {RootState} from "../../reducer"
+import {Translation} from "../../lang/translation"
+
+const Profile: React.FC = function() {
+	const userConnectedRedux = useSelector((state:RootState) => state.userConnected)
+
+  return(
+	<div className="profil connected">
+      <div className="container">
+	      <Header/>
+	      <div className="main-content">
+	      	<div className="main-pro">
+	      	</div>
+	      </div>
+	      <Footer/>
+	  </div>
+    </div>
+  )
 }
 
-export default Router
+export default Profile;
