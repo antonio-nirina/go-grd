f7263078cba28674849434474e0a8389f19427c3
fabyxipe <fabien.rahajaniaina@xipe.com>
2021-05-26 21:58:16 +0200

    add profile annex

--- client/src/views/annexe/profile.tsx
+++ client/src/views/annexe/profile.tsx
@@ -12,6 +12,7 @@ import Assistant from "../assistant/assistant"
 import Communaute from "../communaute/communaute"
 import Login from "../auth/login"
 import Profil from "../profil/profil"
+import Profile from "../annexe/profile"
 import Inscription from "../auth/inscription"
 import InitPass from "../auth/initpass"
 import UpdatePassword from "../auth/updatePassword"
@@ -22,21 +23,22 @@ const Router = function() {
 	return (
 		<Switch>
 			<Route path="/" exact component={Home} />
-			<Route path="/tournament" exact component={Tournament} />
-			<Route path="/info" exact component={Info} />
-			<Route path="/matches" exact component={Matches} />
-			<Route path="/teams" exact component={Teams} />
-			<Route path="/rules" exact component={Rules} />
-			<Route path="/ligue" exact component={Ligue} />
-			<Route path="/wager" exact component={Wager} />
-			<Route path="/communaute" exact component={Communaute} />
-			<Route path="/assistant" exact component={Assistant} />
+			<ProtectedRoute path="/tournament" exact component={Tournament} />
+			<ProtectedRoute path="/info" exact component={Info} />
+			<ProtectedRoute path="/matches" exact component={Matches} />
+			<ProtectedRoute path="/teams" exact component={Teams} />
+			<ProtectedRoute path="/rules" exact component={Rules} />
+			<ProtectedRoute path="/ligue" exact component={Ligue} />
+			<ProtectedRoute path="/wager" exact component={Wager} />
+			<ProtectedRoute path="/communaute" exact component={Communaute} />
+			<ProtectedRoute path="/assistant" exact component={Assistant} />
 			<Route path="/login" exact component={Login} />
 			<Route path="/inscription" exact component={Inscription} />
 			<Route path="/register" exact component={Register} />
 			<Route path="/forgot-password" exact component={InitPass} />
 			<Route path="/update-password" exact component={UpdatePassword} />
-			<Route path="/profil" exact component={Profil} />
+			<ProtectedRoute path="/profil" exact component={Profil} />
+			<ProtectedRoute path="/profile" exact component={Profile} />
 		</Switch>
 	)
 }
