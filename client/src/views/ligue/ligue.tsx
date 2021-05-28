87bdc96ffbd3a3879d08122dde222e0218213318
fabyxipe <fabien.rahajaniaina@xipe.com>
2021-05-28 09:22:24 +0200

    add content ligue

--- client/src/views/ligue/ligue.tsx
+++ client/src/views/ligue/ligue.tsx
@@ -1,10 +1,95 @@
 import React from "react"
+import Header from "../header/header"
+import Footer from "../footer/footer"
+import Community from "../community/community"
+import Join from "../join/join"
+import { useSelector } from "react-redux"
+import { faXbox, faPlaystation } from "@fortawesome/free-brands-svg-icons"
+import { faGamepad } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
+import {Translation} from "../../lang/translation"
+import {RootState} from "../../reducer"
+import "../../assets/css/style.css"
+import "../participate/participate.css"
 
 const Ligue: React.FC = function() {
 
   return(
-  	<>Ligue</>
+  	<div className="container">
+  		<Header/>
+		<div className="participate ligue">
+			<div className="marg">
+				<div className="part">
+					<div className="undertitle">
+						<h2>Ligues</h2>
+						<p>Retrouve les derniers tournois public & inscris-toi !</p>
+					</div>
+					<div className="content">
+						<div className="clear"></div>
+						<div className="apex block">
+							<div>
+								<p className="legend">Apex Legends Daily Cup</p><i className="iconGame"><FontAwesomeIcon icon={faXbox}/></i>
+							</div>
+							<div className="info">
+								<p className="price inblock"><i className="sprite cup"></i><span>100€ Cash Prize</span></p>
+								<p className="date inblock"><i className="sprite calendar"></i><span>02/04/2021 - 5:00 PM</span></p>
+							</div>
+						</div>						
+					<div className="apex block">
+						<div><p className="legend">Fortnite Weekly Cup</p><i className="iconGame"><FontAwesomeIcon icon={faGamepad}/></i></div>
+						<div className="info">
+							<p className="price inblock"><i className="sprite cup"></i><span>50€ Cash Prize</span></p>
+							<p className="date inblock"><i className="sprite calendar"></i><span>03/04/2021 - 5:00 PM</span></p>
+						</div>
+					</div>
+					<div className="apex block">
+						<div><p className="legend">Rocket League Champions</p><i className="iconGame"><FontAwesomeIcon icon={faPlaystation}/></i></div>
+						<div className="info">
+							<p className="price inblock"><i className="sprite ticket"></i><span>5€ Cash Prize</span></p>
+							<p className="price inblock"><i className="sprite cup"></i><span>500€ Cash Prize</span></p>
+							<p className="date inblock"><i className="sprite calendar"></i><span>04/04/2021 - 7:30 PM</span></p>
+						</div>
+					</div>
+					<div className="apex block">
+						<div>
+							<p className="legend">Warzone Xbox Daily</p><i className="iconGame"><FontAwesomeIcon icon={faXbox}/></i>
+						</div>
+						<div className="info">
+							<p className="price inblock"><i className="sprite cup"></i><span>100€ Cash Prize</span></p>
+							<p className="date inblock"><i className="sprite candar"></i><span>02/04/2021 - 5:00 PM</span></p>
+						</div>
+					</div>
+					<div className="apex block">
+						<div><p className="legend">R6 Squad Tournament</p><i className="iconGame"><FontAwesomeIcon icon={faGamepad}/></i></div>
+						<div className="info">
+							<p className="price inblock"><i className="sprite cup"></i><span>100€ Cash Prize</span></p>
+							<p className="date inblock"><i className="sprite calendar"></i><span>02/04/2021 - 5:00 PM</span></p>
+						</div>
+					</div>
+					<div className="apex block last">
+						<p className="legend">Fifa 21 fut cup</p>
+						<div className="info">
+							<p className="inblock"><i className="sprite ticket"></i><span>10€</span></p>
+							<p className="inblock"><i className="sprite cup"></i><span>750€ Cash Prize</span></p>
+							<p className="inblock"><i className="sprite calendar"></i><span>04/04/2021 - 7:30 PM</span></p>
+						</div>
+					</div>
+					</div>
+					<div className="gamecash">
+						<div className="lot">
+							<p><strong>+250</strong><span>Tournois/Semaine</span></p>
+							<p><strong>8</strong><span>Jeux</span></p>
+							<p><strong>+2500€</strong><span>Cash prizes/Semaine</span></p>
+						</div>							
+					</div>
+				</div>						
+			</div>
+		</div>
+		<Community/>
+		<Join/>
+		<Footer/>
+  	</div>
   )
 }
 
