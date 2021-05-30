a59921147ee48a8366de11ddfb61733cb723d76a
antonio-nirina <nirinahantonio@gmail.com>
2021-05-30 09:31:31 +0300

    community

--- client/src/views/communaute/communaute.tsx
+++ client/src/views/communaute/communaute.tsx
@@ -1,9 +1,32 @@
 import React from "react"
 
+import Header from "../header/header"
+import Footer from "../footer/footer"
 
 const Communaute: React.FC = function() {
   return(
-  	<>Communaute</>
+  	<div>
+  		<Header/>
+  		<div className="content">
+  			<div className="side">
+  				<div className="games"></div>
+  				<div className="stream"></div>
+  			</div>
+  			<div className="actuality"></div>
+  			<div>
+  				<div className="friend-inline">
+
+  				</div>
+  				<div className="store">
+
+  				</div>
+  				<div className="tchat">
+
+  				</div>
+  			</div>
+  		</div>
+  		<Footer/>
+  	</div>
   )
 }
 
