f900dd5e2158326b30602ba640371a8477743d87
fabyxipe <fabien.rahajaniaina@xipe.com>
2021-06-05 01:16:55 +0200

    add upload img + add style

--- client/src/views/communaute/communaute.tsx
+++ client/src/views/communaute/communaute.tsx
@@ -1,9 +1,17 @@
 import React from "react"
+import { Link } from 'react-router-dom'
 import { useSelector } from "react-redux"
+
 import { faPlus, faCommentDots, faEye } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {Translation} from "../../lang/translation"
 import warzone from "../../assets/image/warzone-.png"
+import warzoneLogo from "../../assets/image/warzone.png"
+import warz from "../../assets/image/warz.jpg"
+import championship from "../../assets/image/championship.jpeg"
+import fortnite1 from "../../assets/image/fortnite.jpg"
+import rocketleague from "../../assets/image/rocketleague.png"
+import fortnite from "../../assets/image/fortnite.png"
 import rlchampionsip from "../../assets/image/rlchampionsip.png"
 import thumbnail from "../../assets/image/video.png"
 
@@ -25,103 +33,128 @@ const Communaute: React.FC = function() {
 	  			<div className="auto">	  				
 	  				<div className="aside-left">
 	  					<div className="game-select">
-		  					<div className="game-bg bg-cod">
-		  						<p className="game-title">COD : Warzone</p>
-		  					</div>
-		  					<div className="game-bg bg-rl">
-		  						<p className="game-title">rocket league</p>
-		  					</div>
-		  					<div className="game-bg bg-ft">
-		  						<p className="game-title">fortnite</p>
-		  					</div>
+	  						<Link to="#">
+			  					<div className="game-bg wz">
+			  						<p>Warzone</p>
+			  						<div className="seek">
+			  							<img src={warz} className="imgresp"/>
+			  						</div>
+			  					</div>
+			  				</Link>
+			  				<Link to="#">
+			  					<div className="game-bg rl">
+			  						<p>Rocket League</p>
+			  						<div className="seek">
+			  							<img src={championship} className="imgresp"/>
+			  						</div>
+			  					</div>
+		  					</Link>
+		  					<Link to ="#">
+			  					<div className="game-bg ft">
+			  						<p>Fortnite</p>
+			  						<div className="seek">
+			  							<img src={fortnite1} className="imgresp"/>
+			  						</div>
+			  					</div>
+			  				</Link>
 		  				</div>
 		  				<div className="stream">
-		  					<div className="stream-container">
-			  					<div className="streaming">
-			  						<img src={Streamer1} alt=""/>
-			  					</div>
-			  					<div className="stream-info">
-			  						<p className="streamer">Gotaga</p>
-			  						<p className="streamgame">Apex Legends <span className="stream-type">Arena</span></p>
-			  						<p className="view">12093<i><i><FontAwesomeIcon icon={faEye} size="xs"/></i></i></p>
-			  					</div>
-			  				</div>
-			  				<div className="stream-container">
-			  					<div className="streaming">
-			  						<img src={Streamer1} alt=""/>
-			  					</div>
-			  					<div className="stream-info">
-			  						<p className="streamer">Gotaga</p>
-			  						<p className="streamgame">Apex Legends <span className="stream-type">Arena</span></p>
-			  						<p className="view">12093<i><i><FontAwesomeIcon icon={faEye} size="xs"/></i></i></p>
-			  					</div>
-			  				</div>
+		  					<Link to="#">
+			  					<div className="stream-container">
+				  					<div className="streaming">
+				  						<img src={Streamer1} alt=""/>
+				  					</div>
+				  					<div className="stream-info">
+				  						<p className="streamer">Gotaga</p>
+				  						<p className="streamgame">Apex Legends <span className="stream-type">Arena</span></p>
+				  						<p className="view">12093<i><i><FontAwesomeIcon icon={faEye} size="xs"/></i></i></p>
+				  					</div>
+				  				</div>
+			  				</Link>
+			  				<Link to="#">
+				  				<div className="stream-container">
+				  					<div className="streaming">
+				  						<img src={Streamer1} alt=""/>
+				  					</div>
+				  					<div className="stream-info">
+				  						<p className="streamer">Gotaga</p>
+				  						<p className="streamgame">Apex Legends <span className="stream-type">Arena</span></p>
+				  						<p className="view">12093<i><i><FontAwesomeIcon icon={faEye} size="xs"/></i></i></p>
+				  					</div>
+				  				</div>
+				  			</Link>
 		  				</div>
 	  				</div>	  				
 	  				<div className="center-block">
 	  					<h2>Fil d'actualité</h2>
-	  					<div className="actuality">
-          <h3>
-		  {
-				Object.keys(userConnectedRedux.user).length > 0 ?
-				Translation(userConnectedRedux.user.language).participHome.actuality
-				:
-				Translation("fr").participHome.actuality
-			}
-		  </h3>
-          <div className="artContent">
-            <div className="article">
-              <img src={warzone} alt="" />
-              <div className="text">
-                <p className="title">Warzone Patch 1.15.x</p>
-                <p>
-				{
-					Object.keys(userConnectedRedux.user).length > 0 ?
-					Translation(userConnectedRedux.user.language).participHome.retrieve
-					:
-					Translation("fr").participHome.retrieve
-				}
-				</p>
-                <a href="#">
-					{
-						Object.keys(userConnectedRedux.user).length > 0 ?
-						Translation(userConnectedRedux.user.language).participHome.see
-						:
-						Translation("fr").participHome.see
-					}
-				</a>
-              </div>
-            </div>
-            <div className="article">
-              <img src={rlchampionsip} alt="" />
-              <div className="text">
-                <p className="title">Résultats RLCS</p>
-                <p>
-				{
-					Object.keys(userConnectedRedux.user).length > 0 ?
-					Translation(userConnectedRedux.user.language).participHome.follow
-					:
-					Translation("fr").participHome.follow
-				}
-				</p>
-                <a href="#">
-					{
-						Object.keys(userConnectedRedux.user).length > 0 ?
-						Translation(userConnectedRedux.user.language).participHome.see
-						:
-						Translation("fr").participHome.see
-					}
-				</a>
-              </div>
-            </div>
-          </div>
-        </div>
+	  					<div className="actuality">	  						
+							<h3>
+							{
+								Object.keys(userConnectedRedux.user).length > 0 ?
+								Translation(userConnectedRedux.user.language).participHome.actuality
+								:
+								Translation("fr").participHome.actuality
+							}
+							</h3>
+							<div className="artContent">
+								<Link to="/">
+									<div className="article">
+										<img src={warzone} alt="" />
+										<div className="text">
+											<p className="date-art">4 Juin 2021</p>
+											<p className="title">Warzone Patch 1.15.x</p>
+											<p>
+											{
+												Object.keys(userConnectedRedux.user).length > 0 ?
+												Translation(userConnectedRedux.user.language).participHome.retrieve
+												:
+												Translation("fr").participHome.retrieve
+											}
+											</p>
+							                <div className="readmore">
+												{
+													Object.keys(userConnectedRedux.user).length > 0 ?
+													Translation(userConnectedRedux.user.language).participHome.see
+													:
+													Translation("fr").participHome.see
+												}
+											</div>
+										</div>
+		             				</div>
+		             			</Link>
+           					</div>           				
+	           				<Link to="/">
+		            			<div className="article">
+		          					<img src={rlchampionsip} alt="" />
+		          					<div className="text">
+		          						<p className="date-art">5 Juin 2021</p>
+		           						<p className="title">Résultats RLCS</p>
+		            					<p>
+										{
+											Object.keys(userConnectedRedux.user).length > 0 ?
+											Translation(userConnectedRedux.user.language).participHome.follow
+											:
+											Translation("fr").participHome.follow
+										}
+										</p>
+						                <div className="readmore">
+											{
+												Object.keys(userConnectedRedux.user).length > 0 ?
+												Translation(userConnectedRedux.user.language).participHome.see
+												:
+												Translation("fr").participHome.see
+											}
+										</div>
+		          					</div>
+		            			</div>         				
+	         				</Link>
+         				</div>         				        			
 	  				</div>
 	  				<div className="aside-right">
 	  					<div className="friend-list">
 	  						<p>
 	  							<img src={AvatarDefault} className="friend-avatar"/>	  						
-	  							<span>NomAmi</span>
+	  							<span>NomAmi<i className="u-connected"></i></span>
 	  							<i><FontAwesomeIcon icon={faCommentDots} size="xs"/></i>
 	  							<i className="rect"><FontAwesomeIcon icon={faPlus} size="xs"/></i>
 	  						</p>
@@ -129,7 +162,7 @@ const Communaute: React.FC = function() {
 	  					<div className="friend-list">
 	  						<p>
 	  							<img src={AvatarDefault} className="friend-avatar"/>	  						
-	  							<span>NomAmi</span>
+	  							<span>NomAmi<i className="u-disconnected"></i></span>
 	  							<i><FontAwesomeIcon icon={faCommentDots} size="xs"/></i>
 	  							<i className="rect"><FontAwesomeIcon icon={faPlus} size="xs"/></i>
 	  						</p>
@@ -137,7 +170,7 @@ const Communaute: React.FC = function() {
 	  					<div className="friend-list noborder">
 	  						<p>
 	  							<img src={AvatarDefault} className="friend-avatar"/>	  						
-	  							<span>NomAmi</span>
+	  							<span>NomAmi<i className="u-connected"></i></span>
 	  							<i><FontAwesomeIcon icon={faCommentDots} size="xs"/></i>
 	  							<i className="rect"><FontAwesomeIcon icon={faPlus} size="xs"/></i>
 	  						</p>
