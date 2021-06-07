50d7aaf08221fd07821b02306fa641ee2792a2cd
antonio-nirina <nirinahantonio@gmail.com>
2021-06-07 09:14:22 +0300

    test channel

--- front-office/api/go.mod
+++ front-office/api/go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/antonio-nirina/go-grd/api v0.0.0-20210424142750-ff1bb81bd033
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-redis/redis/v8 v8.8.2
-	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/gorilla/websocket v1.4.2
 	github.com/graphql-go/graphql v0.7.9
 	github.com/graphql-go/handler v0.2.3
 	github.com/joho/godotenv v1.3.0
--- front-office/api/main.go
+++ front-office/api/main.go
@@ -1,15 +1,48 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"sync"
+	"time"
 
+	"github.com/gorilla/websocket"
 	"github.com/graphql-go/graphql"
 	"github.com/graphql-go/handler"
 	"github.com/thoussei/antonio/main/front-office/api/external"
 	"github.com/thoussei/antonio/main/front-office/api/graphql/mutation"
 	"github.com/thoussei/antonio/main/front-office/api/graphql/queries"
 )
+
+var counter int
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+	Subprotocols: []string{"graphql-ws"},
+}
+
+type ConnectionACKMessage struct {
+	OperationID string `json:"id,omitempty"`
+	Type        string `json:"type"`
+	Payload     struct {
+		Query string `json:"query"`
+	} `json:"payload,omitempty"`
+}
+
+type Subscriber struct {
+	ID            int
+	Conn          *websocket.Conn
+	RequestString string
+	OperationID   string
+}
+
+var subscribers sync.Map
 
 func main() {
 	schemaConfig := graphql.SchemaConfig{
@@ -21,14 +54,17 @@ func main() {
 			Name:   "Mutation",
 			Fields: mutation.GetRootFields(),
 		}),
-		/*Subscription: graphql.NewObject(graphql.ObjectConfig{
+		Subscription: graphql.NewObject(graphql.ObjectConfig{
             Name: "Subscription",
             Fields: graphql.Fields{
-                "subscribeUser": &graphql.Field{
-                    Type: graphql.String,
+                "subscribeCounter": &graphql.Field{
+                    Type: graphql.Int,
+                    Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+						return counter, nil
+					},
                 },
             },
-        }),*/
+        }),
 	}
 	schema, err := graphql.NewSchema(schemaConfig)
 
@@ -45,6 +81,58 @@ func main() {
 	})
 
 	http.Handle("/", external.Handle(httpHandler))
+	http.HandleFunc("/subscriptions", func(w http.ResponseWriter, r *http.Request) {
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Printf("failed to do websocket upgrade: %v", err)
+			return
+		}
+		connectionACK, err := json.Marshal(map[string]string{
+			"type": "connection_ack",
+		})
+
+		if err != nil {
+			log.Printf("failed to marshal ws connection ack: %v", err)
+		}
+		if err := conn.WriteMessage(websocket.TextMessage, connectionACK); err != nil {
+			log.Printf("failed to write to ws connection: %v", err)
+			return
+		}
+	})
+	go func() {
+		for {
+			time.Sleep(1 * time.Second)
+			counter ++
+			subscribers.Range(func(key, value interface{}) bool {
+				subscriber, ok := value.(*Subscriber)
+				if !ok {
+					return true
+				}
+				payload := graphql.Do(graphql.Params{
+					Schema:        schema,
+					RequestString: subscriber.RequestString,
+				})
+				message, err := json.Marshal(map[string]interface{}{
+					"type":    "data",
+					"id":      subscriber.OperationID,
+					"payload": payload,
+				})
+				if err != nil {
+					log.Printf("failed to marshal message: %v", err)
+					return true
+				}
+				if err := subscriber.Conn.WriteMessage(websocket.TextMessage, message); err != nil {
+					if err == websocket.ErrCloseSent {
+						subscribers.Delete(key)
+						return true
+					}
+					log.Printf("failed to write to ws connection: %v", err)
+					return true
+				}
+				return true
+			})
+		}
+	}()
 	fmt.Println("ready: listening 4000")
 	http.ListenAndServe(":4000", nil)
 }
