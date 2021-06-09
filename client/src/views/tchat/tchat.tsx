e7e4615c010d5167149088bc0535b8e223ae09ea
antonio-nirina <nirinahantonio@gmail.com>
2021-06-09 19:55:37 +0300

    fix config

--- client/src/views/tchat/tchat.tsx
+++ client/src/views/tchat/tchat.tsx
@@ -1,22 +1,12 @@
-import React,{useMemo, useState, useCallback, Component, useEffect} from "react"
+import React,{useMemo, useEffect} from "react"
 import { Widget, addResponseMessage } from 'react-chat-widget'
-
-
-import 'react-chat-widget/lib/styles.css'
-import { Link } from 'react-router-dom'
-import { useSelector } from "react-redux"
 import {useSubscription} from "@apollo/client"
-
-import Header from "../header/header"
 import {RootState} from "../../reducer"
 
-import Footer from "../footer/footer"
+import 'react-chat-widget/lib/styles.css'
 import AvatarDefault from "../../assets/image/game-tag.png"
-import Streamer1 from "../../assets/image/streamer1.jpg"
 import "./tchat.css"
 import {COUNT_SUBSCRIBE} from "../../gql/user/subscription"
-
-
 
 const Tchat: React.FC = function() {
 	useEffect(() => {
@@ -26,7 +16,7 @@ const Tchat: React.FC = function() {
    const handleNewUserMessage = () => {
     
   	};
-	const userConnectedRedux 	= useSelector((state:RootState) => state.userConnected)
+	// const userConnectedRedux 	= useSelector((state:RootState) => state.userConnected)
 	const {loading,error,data}  = useSubscription(COUNT_SUBSCRIBE)
 	useMemo(() => {
 		if(!loading && !error && data) console.log("data", data)
