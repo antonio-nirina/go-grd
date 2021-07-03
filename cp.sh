4e7eb13ebff2ea0f862f13884102b6875eb8ed9a
antonio-nirina <nirinahantonio@gmail.com>
2021-07-03 14:02:41 +0300

    last change directory

--- cp.sh
+++ cp.sh
@@ -6,7 +6,7 @@ orginame="${HOME}/Documents/faceit/new-project/go-example/client"
 ws="${HOME}/Documents/faceit/new-project/grind/ws"
 org_server="${HOME}/Documents/faceit/new-project/go-example/api"
 ws_server="${HOME}/Documents/faceit/new-project/go-example/ws"
-gitlab="${HOME}/Documents/faceit/new-project/go-example/.gitlab-ci.yml"
+gitlab="${HOME}/Documents/faceit/new-project/go-example/gitlab-ci.txt"
 
 if [ -d ${dir} ] ; then
     rm -rf $dir 
