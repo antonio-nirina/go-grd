28984ea6bf5b83ca3540f27aca912f22d87839c3
fabien.rahajaniaina@xipe.com <fabien.rahajaniaina@gmail.com>
2021-07-08 16:42:35 +0200

    fix warning

--- client/src/views/waggers/waggersgame.tsx
+++ client/src/views/waggers/waggersgame.tsx
@@ -1,10 +1,9 @@
 import React from "react"
-import { Link } from "react-router-dom"
 import Header from "../header/header"
 import Footer from "../footer/footer"
 import Join from "../join/join"
-import { faXbox, faPlaystation } from "@fortawesome/free-brands-svg-icons"
-import { faGamepad, faUsers } from "@fortawesome/free-solid-svg-icons"
+
+import { faUsers } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 //import {Translation} from "../../lang/translation"
 //import {RootState} from "../../reducer"
@@ -12,14 +11,6 @@ import "../waggers/waggers.css"
 import "../../assets/css/style.css"
 import "../annexe/tournois.css"
 
-import apexlegends from "../../assets/image/apex-legends.png"
-import fortnite from "../../assets/image/fortnite.png"
-import rainboxsix from "../../assets/image/rainbowsix.png"
-import rocketleague from "../../assets/image/rocketleague.png"
-import cod_Modernwarfare from "../../assets/image/modernwarfare.png"
-import cod_warzone from "../../assets/image/warzone.png"
-import cod_coldwar from "../../assets/image/cod-coldwar.png"
-import fifa from "../../assets/image/fifa21.png"
 
 const WaggersGame: React.FC = function() {
 
