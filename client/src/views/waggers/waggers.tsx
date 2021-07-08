15a6c39e83f929fa7799d99b5e0514800d872331
fabien.rahajaniaina@xipe.com <fabien.rahajaniaina@gmail.com>
2021-07-08 16:33:30 +0200

    update waggers responsive page

--- client/src/views/waggers/waggers.tsx
+++ client/src/views/waggers/waggers.tsx
@@ -33,56 +33,68 @@ const Waggers: React.FC = function() {
 						<h2>Wagers</h2>
 						<p>Derniers résultats en Wagers</p>
 					</div>
-					<div className="content">
+					<div className="content waggers-link">
 						<div className="clear"></div>
-						<div className="apex block dark-red">
-							<div>
-								<p className="legend">Apex Legends Daily Cup</p><i className="iconGame"><FontAwesomeIcon icon={faXbox}/></i>
+						<Link to ="waggers-game">
+							<div className="apex block dark-red">
+								<div>
+									<p className="legend">Apex Legends Daily Cup</p><i className="iconGame"><FontAwesomeIcon icon={faXbox}/></i>
+								</div>
+								<div className="info">
+									<p className="price inblock"><i className="sprite cup"></i><span>100€ Cash Prize</span></p>
+									<p className="date inblock"><i className="sprite calendar"></i><span>02/04/2021 - 5:00 PM</span></p>
+								</div>
 							</div>
+						</Link>
+						<Link to ="#">
+						<div className="apex block dark-red">
+							<div><p className="legend">Fortnite Weekly Cup</p><i className="iconGame"><FontAwesomeIcon icon={faGamepad}/></i></div>
+							<div className="info">
+								<p className="price inblock"><i className="sprite cup"></i><span>50€ Cash Prize</span></p>
+								<p className="date inblock"><i className="sprite calendar"></i><span>03/04/2021 - 5:00 PM</span></p>
+							</div>
+						</div>
+						</Link>
+						<Link to ="#">
+							<div className="apex block light-green">
+								<div><p className="legend">Rocket League Champions</p><i className="iconGame"><FontAwesomeIcon icon={faPlaystation}/></i></div>
+								<div className="info">
+									<p className="price inblock"><i className="sprite ticket"></i><span>5€ Cash Prize</span></p>
+									<p className="price inblock"><i className="sprite cup"></i><span>500€ Cash Prize</span></p>
+									<p className="date inblock"><i className="sprite calendar"></i><span>04/04/2021 - 7:30 PM</span></p>
+								</div>
+							</div>
+						</Link>
+						<Link to ="#">
+							<div className="apex block dark-red">
+								<div>
+									<p className="legend">Warzone Xbox Daily</p><i className="iconGame"><FontAwesomeIcon icon={faXbox}/></i>
+								</div>
+								<div className="info">
+									<p className="price inblock"><i className="sprite cup"></i><span>100€ Cash Prize</span></p>
+									<p className="date inblock"><i className="sprite calendar"></i><span>02/04/2021 - 5:00 PM</span></p>
+								</div>
+							</div>
+						</Link>
+						<Link to ="#">
+							<div className="apex block dark-red">
+							<div><p className="legend">R6 Squad Tournament</p><i className="iconGame"><FontAwesomeIcon icon={faGamepad}/></i></div>
 							<div className="info">
 								<p className="price inblock"><i className="sprite cup"></i><span>100€ Cash Prize</span></p>
 								<p className="date inblock"><i className="sprite calendar"></i><span>02/04/2021 - 5:00 PM</span></p>
 							</div>
-						</div>						
-					<div className="apex block dark-red">
-						<div><p className="legend">Fortnite Weekly Cup</p><i className="iconGame"><FontAwesomeIcon icon={faGamepad}/></i></div>
-						<div className="info">
-							<p className="price inblock"><i className="sprite cup"></i><span>50€ Cash Prize</span></p>
-							<p className="date inblock"><i className="sprite calendar"></i><span>03/04/2021 - 5:00 PM</span></p>
-						</div>
-					</div>
-					<div className="apex block light-green">
-						<div><p className="legend">Rocket League Champions</p><i className="iconGame"><FontAwesomeIcon icon={faPlaystation}/></i></div>
-						<div className="info">
-							<p className="price inblock"><i className="sprite ticket"></i><span>5€ Cash Prize</span></p>
-							<p className="price inblock"><i className="sprite cup"></i><span>500€ Cash Prize</span></p>
-							<p className="date inblock"><i className="sprite calendar"></i><span>04/04/2021 - 7:30 PM</span></p>
-						</div>
-					</div>
-					<div className="apex block dark-red">
-						<div>
-							<p className="legend">Warzone Xbox Daily</p><i className="iconGame"><FontAwesomeIcon icon={faXbox}/></i>
-						</div>
-						<div className="info">
-							<p className="price inblock"><i className="sprite cup"></i><span>100€ Cash Prize</span></p>
-							<p className="date inblock"><i className="sprite calendar"></i><span>02/04/2021 - 5:00 PM</span></p>
-						</div>
-					</div>
-					<div className="apex block dark-red">
-						<div><p className="legend">R6 Squad Tournament</p><i className="iconGame"><FontAwesomeIcon icon={faGamepad}/></i></div>
-						<div className="info">
-							<p className="price inblock"><i className="sprite cup"></i><span>100€ Cash Prize</span></p>
-							<p className="date inblock"><i className="sprite calendar"></i><span>02/04/2021 - 5:00 PM</span></p>
-						</div>
-					</div>
-					<div className="apex block last light-green">
-						<p className="legend">Fifa 21 fut cup</p>
-						<div className="info">
-							<p className="inblock"><i className="sprite ticket"></i><span>10€</span></p>
-							<p className="inblock"><i className="sprite cup"></i><span>750€ Cash Prize</span></p>
-							<p className="inblock"><i className="sprite calendar"></i><span>04/04/2021 - 7:30 PM</span></p>
-						</div>
-					</div>
+							</div>
+						</Link>
+						<Link to ="#">
+							<div className="apex block last light-green">
+								<p className="legend">Fifa 21 fut cup</p>
+								<div className="info">
+									<p className="inblock"><i className="sprite ticket"></i><span>10€</span></p>
+									<p className="inblock"><i className="sprite cup"></i><span>750€ Cash Prize</span></p>
+									<p className="inblock"><i className="sprite calendar"></i><span>04/04/2021 - 7:30 PM</span></p>
+								</div>
+							</div>
+						</Link>
 					</div>								
 				</div>
 			</div>			
