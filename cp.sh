baf7d4793381e5f4a5877ab34bbe7a280f56fe79
antonio-nirina <nirinahantonio@gmail.com>
2021-07-21 09:17:10 +0300

    lang tournament

--- cp.sh
+++ cp.sh
@@ -4,9 +4,12 @@ dir="${HOME}/Documents/faceit/new-project/grind/client"
 server="${HOME}/Documents/faceit/new-project/grind/api"
 orginame="${HOME}/Documents/faceit/new-project/go-example/client"
 ws="${HOME}/Documents/faceit/new-project/grind/ws"
+admin_grd="${HOME}/Documents/faceit/new-project/grind/admin"
 org_server="${HOME}/Documents/faceit/new-project/go-example/api"
 ws_server="${HOME}/Documents/faceit/new-project/go-example/ws"
 gitlab="${HOME}/Documents/faceit/new-project/go-example/.gitlab-ci.yml"
+
+admin="${HOME}/Documents/faceit/new-project/go-example/admin"
 
 if [ -d ${dir} ] ; then
     rm -rf $dir 
@@ -27,6 +30,13 @@ if [  -d ${ws} ] ; then
     cp -r ${ws_server} "${HOME}/Documents/faceit/new-project/grind/"
 else 
     cp -r ${ws_server} "${HOME}/Documents/faceit/new-project/grind/"
+fi
+
+if [  -d ${admin_grd} ] ; then
+    rm -rf $admin_grd
+    cp -r ${admin} "${HOME}/Documents/faceit/new-project/grind/"
+else 
+    cp -r ${admin} "${HOME}/Documents/faceit/new-project/grind/"
 fi
 
 # if [ -f ${gitlab} ] ; then
