6d8128639d309e80707a7e0f2cdd899ba75e063f
antonio-nirina <nirinahantonio@gmail.com>
2021-10-12 00:29:04 +0300

    finalidte admin

--- admin/src/communaute/list-communaute.tsx
+++ admin/src/communaute/list-communaute.tsx
@@ -1,11 +1,13 @@
 import React,{useState,useEffect} from 'react'
 import { Link } from "react-router-dom"
 import Loader from "react-loader-spinner"
+import {useHistory } from "react-router-dom"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {useQuery} from "@apollo/client"
-import { faPlus, faSort,faEdit,faTrash } from "@fortawesome/free-solid-svg-icons"
+import {useQuery,useMutation} from "@apollo/client"
+import { faPlus,faTrash,faCheck } from "@fortawesome/free-solid-svg-icons"
 
 import {GET_ALL_CMTY} from "../gql/cmty/query"
+import {EDIT_STATUT_PUBLICATION,REMOVE_PUBLICATION} from "../gql/cmty/mutation"
 import SideBar from "../header/sidebar"
 import Nav from "../header/nav"
 import {NUMBER_PER_PAGE} from "../common/constante"
@@ -34,8 +36,9 @@ type Stremings = {
 }
 
 const ListCommunaute = function() {
-    const [cmty, setCmty] = useState<any[]>([])
-    const [isLoader, setIsLoader] = useState<Boolean>(true)
+	const history = useHistory()
+    const [cmty, setCmty] = useState<Stremings[]>([])
+    const [isLoader, setIsLoader] = useState<boolean>(true)
     // const [item, setItem] = useState<Item>({item:1})
     const {loading,error,data} 	= useQuery(GET_ALL_CMTY,{
         variables:{
@@ -44,31 +47,38 @@ const ListCommunaute = function() {
         }
     })
 
+	const [updatedPub] = useMutation(EDIT_STATUT_PUBLICATION)
+	const [removedPub]  = useMutation(REMOVE_PUBLICATION)
+
     useEffect(() => {
 		if(!loading && !error && data) {
-            let array:any[] = []
 			setIsLoader(false)
-            data.FindAllCmty.forEach(function(e:Stremings){
-                e.streaming.forEach(element => {
-                    array.push({
-                        uid:e.uid,
-                        nameGame:e.game.name,
-                        imageGame:e.game.box_art_url,
-                        id:element.id,
-                        videoId:element.videoId,
-                        gameId:element.gameId,
-                        title:element.title,
-                        viewerCount:element.viewerCount,
-                        createdAt:element.createdAt,
-                        creatorName:element.creatorName,
-                        thumbnailUrl:element.thumbnailUrl,
-
-                    })
-                });
-            })
-			setCmty(array)
+			setCmty(data.FindAllCmty)
 		}
 	},[loading,error,data,isLoader])
+
+	const handleStatut = async function(element:Stremings) {
+		setIsLoader(true)
+		const result = await updatedPub({ variables: {
+			statut:!element.statut,
+			uid:element.uid,
+		} })
+		if (result.data.EditStatutPublication) {
+			setIsLoader(false)
+			history.push("/admin/list/communaute")
+		}
+	}
+
+	const handleRemoved = async function(element:Stremings) {
+		setIsLoader(true)
+		const result = await removedPub({ variables: {
+			uid:element.uid,
+		} })
+		if (result.data.RemovePublication) {
+			setIsLoader(false)
+			history.push("/admin/list/communaute")
+		}
+	}
 
     return(
         <div className="layout-container">
@@ -90,83 +100,41 @@ const ListCommunaute = function() {
 								</Link>
 							</div>
 						</div>
-						<div className="auto-scroll">
-							<div className="sm-width">
-								<div className="body-card align-center">
-									<div className="card-title">
-										<p>Titre<i><FontAwesomeIcon icon={faSort} size="lg"/></i></p>
-									</div>
-									<div className="card-title">
-										<p>Date création<i><FontAwesomeIcon icon={faSort} size="lg"/></i></p>
-									</div>
-									<div className="card-title">
-										<p>Nombre de vues<i><FontAwesomeIcon icon={faSort} size="lg"/></i></p>
-									</div>
-                                    <div className="card-title">
-										<p>Créateur<i><FontAwesomeIcon icon={faSort} size="lg"/></i></p>
-									</div>
-                                    <div className="card-title">
-										<p>Nom du jeu<i><FontAwesomeIcon icon={faSort} size="lg"/></i></p>
-									</div>
-                                    <div className="card-title">
-										<p>Video<i><FontAwesomeIcon icon={faSort} size="lg"/></i></p>
-									</div>
-									<div className="card-title">
-										<p>
-											Statut
-											<i>
-												<FontAwesomeIcon icon={faSort} size="lg"/>
-											</i>
-										</p>
-									</div>
-									<div className="card-title">
-									</div>
-								</div>
-                                <div className={isLoader ? "loader-spinner":"d-none"}>
-									<Loader
-								        type="Oval"
-								        color="#dd0000"
-								    />
-								</div>
-								{
-									cmty?.map(function(el:any,index:number){
-										return (
-											<div className="body-card" key={index} style={{"cursor":"pointer"}}>
-												<div className="card-result align-center">
-													<p>{el.title}</p>
-												</div>
-												<div className="card-result align-center">
-													<p>{el.createdAt}</p>
-												</div>
-												<div className="card-result align-center">
-													<p>{el.viewerCount}</p>
-												</div>
-                                                <div className="card-result align-center">
-													<p>{el.creatorName}</p>
-												</div>
-                                                <div className="card-result align-center">
-													<p>{el.nameGame}</p>
-												</div>
-                                                <div className="card-result align-center">
-													<p>
-														<img src={el.thumbnailUrl} alt="" />
-													</p>
-												</div>
-												<div className="card-result align-center">
-													<p>{el.statut?"Active":"Inactif"}</p>
-												</div>
-												<div className="card-result align-right">
-													<>
-														<Link to={`/admin/detail/${el.uid}`}><i><FontAwesomeIcon icon={faEdit} /></i></Link>
-														<i><FontAwesomeIcon icon={faTrash} /></i>
-													</>
-												</div>
-											</div>
-										)
-									})
-								}
-							</div>
+						<div className={isLoader ? "loader-spinner":"d-none"}>
+							<Loader
+								type="Oval"
+								color="#dd0000"
+							/>
 						</div>
+						{
+							cmty?.map(function(el:Stremings,index:number){
+								return (
+									<div key={index}>
+										<div style={{"float":"right"}}>
+										<button className="btn bg-green" style={{"cursor":"pointer"}} onClick={() => handleStatut(el)}>
+												<FontAwesomeIcon icon={faCheck} />
+												{el.statut? "Désactiver":"Activer"}
+											</button>
+											<span style={{"cursor":"pointer","marginLeft":"12px"}} onClick={() => handleRemoved(el)} ><i><FontAwesomeIcon icon={faTrash} /></i></span>
+										</div>
+										<div  style={{"border": "1px solid #d00","marginTop":"12px","clear":"both"}}>
+											{el.streaming.map(function(ev:any) {
+												return (<span className="grid league-board">
+													<p>Title: {ev.title}</p>
+													<p>Date: {ev.createdAt}</p>
+													<p>Createur: {ev.creatorName}</p>
+													<p>View: {ev.viewerCount}</p>
+													<p>Game: {el.game.name}</p>
+													<p>
+														<img src={ev.thumbnailUrl} style={{"width":"100%"}} alt={ev.nameGame} />
+													</p>
+												</span>)
+											})}
+										</div>
+									</div>
+								)
+							})
+						}
 					</div>
 				</div>
 			</div>
--- client/src/views/communaute/communaute.tsx
+++ client/src/views/communaute/communaute.tsx
@@ -1,26 +1,46 @@
-import React,{useMemo} from "react"
-import { Link } from 'react-router-dom'
+import React,{useMemo,useState,useEffect} from "react"
 import { useSelector } from "react-redux"
 import {useQuery} from "@apollo/client"
 import { faEye } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import Loader from "react-loader-spinner"
 import Header from "../header/header"
 import {RootState} from "../../reducer"
 
 import Footer from "../footer/footer"
-import Streamer1 from "../../assets/image/streamer1.jpg"
 import "./communaute.css"
 import "./post.css"
 // import {COUNT_SUBSCRIBE} from "../../gql/user/subscription"
 import {GET_ALL_STREAMING} from "../../gql/user/query"
+import {GET_ALL_CMTY} from "../../gql/cmty/query"
 import Friend from "./friends"
-
 
 import Post from "./post"
 
+type Stremings = {
+    uid:string
+    statut:string
+    streaming:[{
+        id:string
+        videoId:string
+        gameId:string
+        title:string
+        viewerCount:number
+        createdAt:string
+        creatorName:string
+        thumbnailUrl:string
+    }]
+    game:{
+        uid:string
+        name:string
+        box_art_url:string
+    }
+}
+
 const Communaute: React.FC = function() {
 	const userConnectedRedux 	= useSelector((state:RootState) => state.userConnected)
+	const [cmty, setCmty] = useState<Stremings[]>([])
+	const [isLoader, setIsLoader] = useState<boolean>(true)
 	// const {loading,error,data}  = useSubscription(COUNT_SUBSCRIBE)
 
 	const {loading:loadingTwitch,error:errorTwitch,data:dataTwitch} = useQuery(GET_ALL_STREAMING, {
@@ -29,9 +49,24 @@ const Communaute: React.FC = function() {
 		},
 	})
 
+	const {loading,error,data} 	= useQuery(GET_ALL_CMTY,{
+        variables:{
+            limit:5,
+			pageNumber:1, //(item.item)*NUMBER_PER_PAGE - NUMBER_PER_PAGE
+        }
+    })
+
 	useMemo(() => {
 		if(!loadingTwitch && !errorTwitch && dataTwitch) console.log("dataTwitch", dataTwitch)
 	},[loadingTwitch,errorTwitch,dataTwitch])
+
+	useEffect(() => {
+		if(!loading && !error && data) {
+			setIsLoader(false)
+			const newData = data.FindAllCmty.filter((el:Stremings) => {return el.statut})
+			setCmty(newData)
+		}
+	},[loading,error,data,isLoader])
 
   return(
 	<div className="communaute">
@@ -69,34 +104,36 @@ const Communaute: React.FC = function() {
 		  				</div>
 		  				*/}
 		  				<div className="stream">
-		  					<Link to="#">
-			  					<div className="stream-container">
-				  					<div className="streaming">
-				  						<img src={Streamer1} alt=""/>
-				  					</div>
-				  					<div className="stream-info">
-				  						<p className="streamer">Gotaga</p>
-				  						<p className="streamgame">Apex Legends <span className="stream-type">Arena</span></p>
-				  						<p className="view">12093<i><i><FontAwesomeIcon icon={faEye} size="xs"/></i></i></p>
-				  					</div>
-				  				</div>
-			  				</Link>
-			  				<Link to="#">
-				  				<div className="stream-container">
-				  					<div className="streaming">
-				  						<img src={Streamer1} alt=""/>
-				  					</div>
-				  					<div className="stream-info">
-				  						<p className="streamer">Gotaga</p>
-				  						<p className="streamgame">Apex Legends <span className="stream-type">Arena</span></p>
-				  						<p className="view">12093<i><i><FontAwesomeIcon icon={faEye} size="xs"/></i></i></p>
-				  					</div>
-				  				</div>
-				  			</Link>
+						  	{cmty.map(function(e:Stremings){
+								return(
+									e.streaming.map(function(ev:any,index:number) {
+										return(
+											<span key={index}>
+												<div className="stream-container">
+													<div className="streaming">
+													<img src={ev.thumbnailUrl} style={{"width":"100%"}} alt={ev.nameGame} />
+													</div>
+													<div className="stream-info">
+														<p className="streamer">{ev.creatorName}</p>
+														<p className="streamgame">{e.game.name} <span className="stream-type">Arena</span></p>
+														<p className="view">{ev.viewerCount}<i><i><FontAwesomeIcon icon={faEye} size="xs"/></i></i></p>
+													</div>
+												</div>
+											</span>
+										)
+									})
+								)
+							})}
 		  				</div>
 	  				</div>
 	  				<div className="center-block">
 	  					<Post />
+						  <div className={isLoader ? "loader-spinner":"d-none"}>
+							<Loader
+								type="Oval"
+								color="#dd0000"
+							/>
+						</div>
 	  				</div>
 
 	  				<Friend />
