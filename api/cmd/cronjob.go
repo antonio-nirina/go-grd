0cd702ac0fe76cd05b280cd507fc8d8a1afd998b
antonio-nirina <nirinahantonio@gmail.com>
2022-01-12 00:19:45 +0300

    set cron to queue job

--- api/cmd/cronjob.go
+++ api/cmd/cronjob.go
@@ -1,13 +1,22 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"time"
 
 	"github.com/go-co-op/gocron"
+	"github.com/thoussei/antonio/api/external"
 	"github.com/thoussei/antonio/api/graphql/queries"
 	"github.com/thoussei/antonio/api/tournament/repository"
 )
+
+type jobTournament struct {
+	Uid        string   `json:"uid"`
+	Title       string   `json:"title"`
+	DateStart   string   `json:"dateStart"`
+	DeadlineDate string `json:"deadlineDate"`
+}
 
 func RunCheckTournament() {
 	var tournamentRepository = repository.NewTournamentRepository(queries.Database)
@@ -19,15 +28,25 @@ func RunCheckTournament() {
 	s := gocron.NewScheduler(time.UTC)
 	fmt.Println("tournamentNow", tournamentNow)
 	
-	if len(tournamentNow) == 0 {
-		j, _ := s.Every(10).Seconds().Do(func() { time.Sleep(2 * time.Second) })
-		fmt.Println(j.IsRunning())
+	if len(tournamentNow) > 0 {
+		for _,tournament := range tournamentNow {
+			job := &jobTournament{Uid: tournament.Uid.Hex(),Title: tournament.Title,DateStart: tournament.DateStart,DeadlineDate:tournament.DeadlineDate}
+			json,err := json.Marshal(job)
+			
+			if err != nil {
+				external.Logger(err.Error())
+			}
+			
+			task, _ := s.Every(1).Day().At("01:10").Do(func ()  {
+				external.RPushRedis("default"+tournament.Uid.Hex(),json)
+			})
+			fmt.Println(task.ScheduledTime())
+			fmt.Println("Tournament ",tournament.Title)
+		}
+		
 		s.StartAsync()
-		time.Sleep(time.Second)
-		fmt.Println(j.IsRunning())
-
-		time.Sleep(time.Second)
 		s.Stop()
 	}
-	fmt.Println("Run is Task")
+
+	fmt.Println("Tournament isFinished run")
 }
