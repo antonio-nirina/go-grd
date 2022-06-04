05488abef8915d961ae66a1adca705ff9a6ab136
nirina-antonio <nirinahantonio@gmail.com>
2022-06-04 23:12:28 +0200

    architecture general optimizer

--- server/handlers/handler.go
+++ server/handlers/handler.go
@@ -7,5 +7,17 @@ import (
 )
 
 func MethodHandler(ctx *gin.Context) {
-	ctx.String(http.StatusOK, "Hello xxxxx")
+	var resp Response
+	var array = make(map[string]interface{})
+	var data []map[string]interface{}
+	data = append(data, array)
+	array["1"] = "Text1"
+	array["2"] = "Text2"
+	array["3"] = "Text3"
+	array["4"] = "Text4"
+	resp.Code = 200
+	resp.Message = "success"
+	resp.Data = data
+
+	ctx.JSON(http.StatusOK, resp)
 }
---
+++ server/handlers/response.go
@@ -0,0 +1,8 @@
+package handlers
+
+// Response Type
+type Response struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+	Data    []map[string]interface{}
+}
