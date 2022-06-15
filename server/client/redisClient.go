077ed13062a5fb41c191ad7daa14715e4dc2d5eb
nirina-antonio <nirinahantonio@gmail.com>
2022-06-16 00:54:21 +0200

    redis client implement

---
+++ server/client/redisClient.go
@@ -0,0 +1,104 @@
+package client
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/joho/godotenv"
+	"github.com/thoussei/antonio/server/log"
+)
+
+type UserRed struct {
+	Username string
+	Email    string
+}
+
+var Rdb *redis.Client
+var ctx = context.Background()
+
+func init() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.LogFormater(fmt.Sprintf("%v", err))
+	}
+
+	var host = fmt.Sprintf("%s:%s", os.Getenv("HOST_REDIS"), os.Getenv("PORT_REDIS"))
+	Rdb = redis.NewClient(&redis.Options{
+		Addr:     host,
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+}
+
+func GetDataRedis(key string) (interface{}, error) {
+	data, err := Rdb.Get(ctx, key).Result()
+
+	if err == redis.Nil {
+		return nil, err
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func SetDataRedis(key string, value interface{}) {
+	p, err := json.Marshal(value)
+
+	if err != nil {
+		log.LogFormater(fmt.Sprintf("%v", err))
+	}
+
+	err = Rdb.Set(ctx, key, p, 0).Err()
+
+	if err != nil {
+		log.LogFormater(fmt.Sprintf("%v", err))
+	}
+}
+
+// - HSet("myhash", map[string]interface{}{"key1": "value1", "key2": "value2"})
+func SetHmsetRedis(hash string, key string, value interface{}) {
+	err := Rdb.HSet(ctx, hash, key, value).Err()
+	if err != nil {
+		log.LogFormater(fmt.Sprintf("%v", err))
+	}
+}
+
+func GetHmsetRedis(key string, field string) ([]interface{}, error) {
+	data, err := Rdb.HMGet(ctx, key, field).Result()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func RemoveHmsetRedis(key string, field string) {
+	err := Rdb.HDel(ctx, key, field).Err()
+	if err != nil {
+		log.LogFormater(fmt.Sprintf("%v", err))
+	}
+}
+
+func RPushRedis(key string, value interface{}) {
+	err := Rdb.RPush(ctx, key, value).Err()
+	if err != nil {
+		fmt.Println("Error", err)
+		log.LogFormater(fmt.Sprintf("%v", err))
+	}
+}
+
+func RPublishTournamentOfDay(key string, value interface{}) {
+	err := Rdb.Publish(ctx, key, value).Err()
+	if err != nil {
+		fmt.Println("Error", err)
+		log.LogFormater(fmt.Sprintf("%v", err))
+	}
+}
