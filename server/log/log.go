cf11551eb85338debfed6971bfe729e76c9942b0
nirina-antonio <nirinahantonio@gmail.com>
2022-06-16 23:05:13 +0200

    change log

--- server/log/log.go
+++ server/log/log.go
@@ -12,7 +12,7 @@ var log = logrus.New()
 func LogFormater(message string) {
 	log.Out = os.Stdout
 	log.SetFormatter(&logrus.TextFormatter{})
-	now := time.Now().UTC().Format("2006-1-1 00:00:00")
+	now := time.Now().UTC().Format("2006-1-1")
 	filename := now + "-log.log"
 	// You could set this to any `io.Writer` such as a file
 	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
