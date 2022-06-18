ba692478f8358f0e50e82a1cc6f2027507e5ca1d
nirina-antonio <nirinahantonio@gmail.com>
2022-06-18 23:05:58 +0200

    fixed log file all request

--- server/log/log.go
+++ server/log/log.go
@@ -1,7 +1,9 @@
 package log
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/sirupsen/logrus"
@@ -12,15 +14,37 @@ var log = logrus.New()
 func LogFormater(message string) {
 	log.Out = os.Stdout
 	log.SetFormatter(&logrus.TextFormatter{})
-	now := time.Now().UTC().Format("2006-1-1")
-	filename, err := os.Create("server/log/data/" + now + "-log.log")
+	now := time.Now().UTC().Format("2006-01-01")
+	path, err := os.Getwd()
 	if err != nil {
-		log.Fatal(err)
+		log.Println("error msg path", err)
 	}
+
+	//Create output path
+	outPath := filepath.Join(path, "server/log/data/")
+	fmt.Println("pathhh", outPath)
+	//Create dir output using above code
+
+	if _, err := os.Stat(outPath); os.IsNotExist(err) {
+		err = os.Mkdir(outPath, 0755)
+		if err != nil {
+			log.Println("error created dir", err)
+		}
+	}
+
+	if err != nil {
+		log.Println("error msg created dir", err)
+	}
+
+	filename, err := os.Create(outPath + now + "-log.log")
+	if err != nil {
+		log.Println("error msg create file", err)
+	}
+
 	// You could set this to any `io.Writer` such as a file
 	file, err := os.OpenFile(filename.Name(), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err == nil {
-		log.Fatal("Error in file ", err)
+		log.Println("error msg open file", err)
 	}
 
 	log.Out = file
