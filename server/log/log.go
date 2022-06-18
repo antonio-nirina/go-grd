19baa212d6a3312388964f034bd5434cb7f8bfff
nirina-antonio <nirinahantonio@gmail.com>
2022-06-18 16:21:27 +0200

    fixed log system

--- server/log/log.go
+++ server/log/log.go
@@ -13,23 +13,12 @@ func LogFormater(message string) {
 	log.Out = os.Stdout
 	log.SetFormatter(&logrus.TextFormatter{})
 	now := time.Now().UTC().Format("2006-1-1")
-	dirname := "server/log/data"
-	err := os.MkdirAll(dirname, 0755)
+	filename, err := os.Create("server/log/data/" + now + "-log.log")
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	dir, err := os.ReadDir(dirname)
-	if err != nil {
-		log.Fatal(err)
-	}
-	filename := ""
-	if dir[0].IsDir() {
-		filename = dir[0].Name() + "/" + now + "-log.log"
-	}
-
 	// You could set this to any `io.Writer` such as a file
-	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	file, err := os.OpenFile(filename.Name(), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err == nil {
 		log.Fatal("Error in file ", err)
 	}
